9e3bf520eebffdde30b14f3bcb9b39e5d5a3f8e2
IJke <ijke@overscope.net>
2018-06-05 13:56:15 +0200

    ha

--- BlackTest/ViewController.swift
+++ BlackTest/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func getFucked() {
+        let hello = 2
+    }
 
 
 }
