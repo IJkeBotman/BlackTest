5737484d37d3e7ac0da1427140c8abe337ce54f8
IJke <ijke@overscope.net>
2018-06-05 13:55:04 +0200

    Bla

--- BlackTest/ViewController.swift
+++ BlackTest/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fuckYou = "Gennaro"
+        for _ in fuckYou {
+            print("ffuccccck")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
